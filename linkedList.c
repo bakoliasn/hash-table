b942318941886da1fb21f15112b4fe9b175ddf15
nicholas <bakoliasn@gmail.com>
2020-04-05 13:41:41 -0400

    implement linked list

---
+++ linkedList.c
@@ -0,0 +1,30 @@
+
+#include<stdlib.h>
+struct LinkedList{
+	int datum;
+	struct LinkedList
+*next;
+};
+typedef struct LunkedList
+*node;
+node newNode(){
+	node temp;
+	temp = (node)malloc(sizeof(struct LibkedList))
+	temp -> next = NULL;
+	return temp;
+	};
+node add(node head, int val){
+	node temp, parent;
+	temp = createNode();
+	temp -> datum = val;
+	if(head == NULL){
+		head = temp;
+	} else {
+		parent = head;
+		while (parent-> next != NULL){
+			p=p->next;
+		}
+		p->next=temp
+	}
+	return head;
+}
